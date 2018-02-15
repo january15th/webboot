f021f4852b5246013505de973ea14c4e023019ca
january15th <reggiechapple@icloud.com>
2018-02-14 18:26:58 -0800

    init

---
+++ src/main/java/com/january15th/webboot/WebbootApplication.java
@@ -0,0 +1,12 @@
+package com.january15th.webboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/january15th/webboot/WebbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.january15th.webboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
